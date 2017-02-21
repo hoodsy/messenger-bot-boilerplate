0c84103adecc713666ca693413ab417b12fa91bb
Logan Bernard <bernard.logan4@gmail.com>
2017-02-21 14:27:17 -0500

    add deploy:dev

--- src/config/index.js
+++ src/config/index.js
@@ -9,6 +9,8 @@ export const db = {
 export const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN
 export const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN
 export const FB_APP_SECRET = process.env.FB_APP_SECRET
+export const FB_PAGE_TEST_TOKEN = process.env.FB_PAGE_TEST_TOKEN
+export const FB_APP_TEST_SECRET = process.env.FB_APP_TEST_SECRET
 export const WIT_TOKEN = process.env.WIT_TOKEN
 const DASHBOT_KEY = process.env.DASHBOT_KEY
 
